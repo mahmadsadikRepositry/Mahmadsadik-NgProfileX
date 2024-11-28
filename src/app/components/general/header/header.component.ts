bc0a408d5e9a0f578c2b0c3640406ca9a9f629ec
sadikkoliya@gmail.com <sadikkoliya@gmail.com>
2024-11-27 22:26:34 -0500

    update analytics call to the component.

--- src/app/components/general/header/header.component.ts
+++ src/app/components/general/header/header.component.ts
@@ -61,6 +61,7 @@ export class HeaderComponent implements OnInit {
   }
 
   downloadCV(){
+    this.analyticsService.sendAnalyticEvent("download_resume", "Download_Resume", "click")
     this.languageService.translateService.get("Header.cvName").subscribe(val => {
       this.cvName = val
       console.log(val)
